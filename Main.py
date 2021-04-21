26a5c5b04e173da37971831d36d0d3f373cc4211
BuildTools <unconfigured@null.spigotmc.org>
2021-04-21 17:51:53 +0100

    Finishe YoutubeAPI.py May need some bug fixing

--- Main.py
+++ Main.py
@@ -1,8 +1,12 @@
 import discord
 
 from discord.ext import commands, tasks
+from discord.utils import get
 import cogs.TwitchAPI as TwitchAPI
+import cogs.YoutubeAPI as YoutubeAPI
 
+global store
+store = 0
 prefix = '$'
 
 client = commands.Bot(command_prefix=prefix)
@@ -12,32 +16,38 @@ client = commands.Bot(command_prefix=prefix)
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     await client.change_presence(activity=discord.Game("Message me for help"))
+    Youtube.start()
 
 
 @client.command()
 async def shutdown(ctx):
     print("Recieved shutdown command")
     await ctx.send("Shutting the bot down")
+    Youtube.stop
     await client.close()
 
 
 async def send(message):
-    await client.send_message(client.get_channel("834168559843147816", message))
+    channel = client.get_channel(834168559843147816)
+    await channel.send(message)
 
 
 @tasks.loop(seconds=30)
 async def Youtube():
+    global store
+    # mentionYT = get(client. name="YT Alerts")
     print("Checking Youtube")
     num = YoutubeAPI.check('123hotdog1100')
     num = int(num)
     print(num)
     if num > store:
-        store = num
-        await send(num)
+        test = "Demomute Just uploaded!! ", YoutubeAPI.conversion("demomute"), " <@&834169017480642572>"
+        str = ''.join(test)
+
+        await send(str)
 
 
 # client.load_extension("cogs.loop")
 
 
-store = 0
 client.run('ODM0MjA0MDg1MzU0NzU4MTY1.YH9fGA.niuPLQf2pf00IpQApzqvofvHt_k')
--- cogs/YoutubeAPI.py
+++ cogs/YoutubeAPI.py
@@ -4,6 +4,7 @@ api_key = 'AIzaSyDtwKl2tY4NXdKRrJ139yLvUKk7qQD2vww'
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
+id = 0
 
 
 def check(Username):
@@ -12,8 +13,46 @@ def check(Username):
         forUsername=Username
     )
     response = request.execute()
+    print(response)
+    for item in response['items']:
+        id = item['id']
 
     for item in response['items']:
         num = item['statistics']['videoCount']
         current = int(num)
-        return(current)
+        return (current)
+
+
+def getid(Username):
+    request = youtube.channels().list(
+        part='statistics',
+        id=Username
+    )
+    response = request.execute()
+    for item in response['items']:
+        id = item['id']
+        return id
+
+
+def latestvid(id):
+    request = youtube.search().list(
+        part='snippet',
+        channelId=id,
+        type='video'
+    )
+    response = request.execute()
+    print(response)
+    for item in response['items']:
+        vid = item['id']['videoId']
+        return vid
+#latestvid("UCQawg27ajRWDNuN-0ieeOrw")
+
+def conversion(Username):
+    BASE_URL = "https://www.youtube.com/watch?v="
+    VIDID = latestvid("UCQawg27ajRWDNuN-0ieeOrw")
+    VIDURL = BASE_URL, VIDID
+    str = ''.join(VIDURL)
+    return str
+
+print(conversion("demomute"))
+
