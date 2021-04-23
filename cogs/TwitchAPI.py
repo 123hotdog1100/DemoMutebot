8ae097eb28dec0a471bead06bc5ab0be53e63693
BuildTools <unconfigured@null.spigotmc.org>
2021-04-23 17:24:59 +0100

    Working on twitchAPI

--- cogs/TwitchAPI.py
+++ cogs/TwitchAPI.py
@@ -20,16 +20,17 @@ def getOauth():##This function gets the oauth2 token which is required for inter
         print(e)
 
 
-def checkUser(userID):##Checks to see if someone is live
-
+def checkUser(userID, OAUTH):##Checks to see if someone is live
+    HEADERS = {'client-id': CLIENT_ID, 'Authorization': 'Bearer ' + OAUTH}
+    URL = BASE_URL + 'streams?user_login=hotdog1100'
     try:
-        req = requests.get(BASE_URL, HEADERS)
+        req = requests.get(URL, headers=HEADERS)
         jsondata = req.json()
-        if 'stream' in jsondata:
-            if jsondata['stream'] is not None:
-                return True
-            else:
-                return False
+        print(jsondata)
+        if len(jsondata['data']) == 1:
+            print('hotdog1100' + ' is live: ' + jsondata['data'][0]['title'] + ' playing ' + jsondata['data'][0]['game_name'])
+        else:
+            return False
     except Exception as e:
         print("Error checking user: ", e)
         return False
@@ -37,18 +38,23 @@ def checkUser(userID):##Checks to see if someone is live
 
 def getUserID(Username, OAUTH):##Gets the USERSID based on the Username they are given and also uses the OAUTH token generated above
     HEADERS = {'client-id': CLIENT_ID, 'Authorization': 'Bearer ' + OAUTH}
-    print(HEADERS)
-    URL = BASE_URL + "users?login="
+    URL = BASE_URL + "users?login=" + Username
     try:
-        req = requests.get(URL, HEADERS)
-        print(req)
+        req = requests.get(URL, headers=HEADERS)
         jsondata = req.json()
         print(jsondata)
+        if len(jsondata['data']) == 1:
+            ID = jsondata['id']
+            return ID
 
     except Exception as e:
-        print("Error getting ID for: ", Username, "Caused by: ", e)
+        print(e)
+
+    #except Exception as e:
+     #   print("Error getting ID for: ", Username, "Caused by: ", e)
 
 
-#AUTH = getOauth()
+AUTH = getOauth()
 
-#getUserID("demomute", AUTH)
+#print(getUserID("hotdog1100", AUTH))
+print(checkUser(75334882, AUTH))
