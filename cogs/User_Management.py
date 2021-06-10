8a3f9287c8e48736a0730ddb8ed01796b0b31abe
123hotdog1100 <123hotdog1100@gmail.com>
2021-06-10 17:02:34 +0100

    Added Temp ban command
    Signed-off-by: 123hotdog1100 <123hotdog1100@gmail.com>

--- cogs/User_Management.py
+++ cogs/User_Management.py
@@ -31,7 +31,7 @@ class User_Management(commands.Cog):
 
     @commands.command(alias="ban", brief="ban a user")
     @commands.has_permissions(ban_members=True)
-    async def Ban(self, ctx, member: discord.Member, *, reason=None):
+    async def Ban(self, ctx, member: discord.Member,*, reason=None):
         if reason == None:
             reason = "No reason given"
         output = f"The moderator {ctx.author.mention} has kicked {member} for {reason}"
@@ -44,9 +44,33 @@ class User_Management(commands.Cog):
         await ctx.message.delete()
         await self.client.get_channel(834074140284813333).send(embed=embedvar)
 
+    @commands.command(alias="tempban", brief="tempbans a user", description="This command tempbans a user")
+    @commands.has_permissions(ban_members=True)
+    async def Tempban(self, ctx, member: discord.Member, time, *, reason=None):
+        time_convert = {"s": 1, "m": 60, "h": 3600, "d": 86400}
+        tempban = int(time[0]) * time_convert[time[-1]]
+        if reason == None:
+            reason = "No reason given"
+        output = f"The moderator {ctx.author.mention} has Temp banned {member} for the duration of {time} and for the reason: {reason}"
+        timeout = f"The ban for {member} has timed out and there for they have been unbanned"
+        embedvar = discord.Embed(title="Tempban output")
+        embedvar.add_field(name="Issue", value=output)
+        response = await ctx.send(embed=embedvar)
+        embedvar2 = discord.Embed(title="tempban timeout")
+        embedvar2.add_field(name="repsonse", value=timeout)
+        await member.ban(reason=reason)
+        await asyncio.sleep(2)
+        await response.delete()
+        await ctx.message.delete()
+        await self.client.get_channel(834074140284813333).send(embed=embedvar)
+        await asyncio.sleep(tempban)
+        await ctx.guild.unban(member)
+        await self.client.get_channel(834074140284813333).send(embed=embedvar2)
+
 
     @Cog.listener()
     async def on_command_error(self, ctx, error):
+        print(error)
         if isinstance(error, discord.ext.commands.errors.MissingPermissions):
             response = await ctx.send("You do not have the required permissions for this command")
             await asyncio.sleep(2)
@@ -54,6 +78,11 @@ class User_Management(commands.Cog):
             print(f"{ctx.author} Tried to use a elevated command in User_Management.py")
             await self.client.get_channel(834074140284813333).send(
                 f"{ctx.author} Tried to use a elevated command in User Management Cog")
+        if isinstance(error, discord.ext.commands.errors.MissingRequiredArgument):
+            response = await ctx.send("This command requires a member and time arguments")
+            await asyncio.sleep(4)
+            await response.delete()
+
 
 
 def setup(client):
