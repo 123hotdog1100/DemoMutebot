c965c251aec928653884916d88197074b2a3af7d
123hotdog1100 <123hotdog1100@gmail.com>
2021-06-10 13:33:53 +0100

    Made it so users can only have one help channel at a time
    Signed-off-by: 123hotdog1100 <123hotdog1100@gmail.com>

--- cogs/Private_Messages.py
+++ cogs/Private_Messages.py
@@ -16,6 +16,8 @@ class Help_Requests(commands.Cog):
         overwrite = PermissionOverwrite()
         overwrite.send_messages = True
         overwrite.read_messages = True
+        senddm = "I have created a help channel for you mods will talk to you in there"
+        category = discord.utils.get(guild.categories, name="Help")
         try:
             if message.channel.id == message.author.dm_channel.id:  # Checks to see if the message was sent in a
                 # private message
@@ -24,23 +26,37 @@ class Help_Requests(commands.Cog):
                 username = message.author.display_name
                 channame = f'Help for {username}'
                 channamecheck = f'help-for-{username}'
-                category = discord.utils.get(guild.categories, name="Help")
-                await guild.create_text_channel(channame, category=category)
+                for channels in guild.channels:
+                    if channels.name == channamecheck:
+                        check = True
+                        break
+                    elif channels.name != channamecheck:
+                        check = False
+
+                if check == True:
+                    await dms.send("You already have a support case please message in that channel")
+                elif check == False:
+                    await guild.create_text_channel(channame, category=category)
+                    await dms.send(senddm)
+
                 for channel in guild.channels:
                     if channel.name == channamecheck:
-                        print(channel.id)
                         chan = self.client.get_channel(channel.id)
                         await chan.set_permissions(message.author, overwrite=overwrite)
 
-                senddm = "I have created a help channel for you mods will talk to you in there"
-                await dms.send(senddm)
-                send = message.author.mention + " Needs help with: " + message.content
-                print(send)  # Out puts message to console for logging reasons
+                send = "Please Help with: " + message.content
+                if check == True:
+                    embedvar = discord.Embed(title="Help Request")
+                    embedvar.add_field(name="Issue", value=send)
+                    await channel.send(embed=embedvar)
+                    pass
+                else:
 
-                embedvar = discord.Embed(title="Help Request")
-                embedvar.add_field(name="Issue", value=send)
-                await channel.send("<@&833822769048977409>")
-                await channel.send(embed=embedvar)
+                    embedvar = discord.Embed(title="Help Request")
+                    embedvar.add_field(name="Issue", value=send)
+                    await channel.send("<@&833822769048977409>")
+                    await channel.send(f"Mods will be with you shortly, {message.author.mention}")
+                    await channel.send(embed=embedvar)
             else:
                 return
         except Exception as e:  # Catches exceptions so the bot doesn't crash out
@@ -61,7 +77,6 @@ class Help_Requests(commands.Cog):
         else:
             await ctx.send("I can only close Help channels")
             pass
-
 
 
 def setup(client):
