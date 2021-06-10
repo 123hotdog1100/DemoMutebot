d859b5c9ba33539bedfe1119becfbe9d8e7308be
123hotdog1100 <123hotdog1100@gmail.com>
2021-06-10 15:18:10 +0100

    Created kick and ban commands need to sort out role checking
    Signed-off-by: 123hotdog1100 <123hotdog1100@gmail.com>

--- cogs/User_Management.py
+++ cogs/User_Management.py
@@ -7,11 +7,27 @@ from discord.ext import commands
 class User_Management(commands.Cog):
     def __init__(self, client):
         self.client = client
+        guild = self.client.get_guild(833822533136416808)
 
-    @commands.command(alias="kick")
-    async def Kick(self, ctx, member : discord.Member):
-        pass
+    @commands.command(alias="kick", brief="kick a user")
+    async def Kick(self, ctx, member: discord.Member, *, reason=None):
+        admin = discord.utils.get(ctx.guild.roles, name=":)")  # Gets the role ":)" from the server
+        mods = discord.utils.get(ctx.guild.roles, name="Mods")  # Gets the role ":)" from the server
+        output = f"The user{ctx.author.mention} has kicked {member} for {reason}"
+        if admin or mods in ctx.author.roles:
+            await member.kick(reason=reason)
+            embedvar = discord.Embed(title="Kick output")
+            embedvar.add_field(name="Issue", value=output)
+            await ctx.send(embed=embedvar)
+        else:
+            ctx.send("You do not have permission to use this command")
+            pass
 
+
+
+    @commands.command(alias="ban", brief="ban a user")
+    async def Ban(self, ctx, member: discord.Member, *, reason=None):
+        await member.ban(reason=reason)
 
 
 def setup(client):
