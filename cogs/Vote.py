8a67fb6a0cb8319a79c9b8fac3ad407bc24d7acd
123hotdog1100 <123hotdog1100@gmail.com>
2021-07-14 23:11:20 +0100

    Added better time display to the voting systemvel
    Signed-off-by: 123hotdog1100 <123hotdog1100@gmail.com>

--- cogs/Vote.py
+++ cogs/Vote.py
@@ -1,6 +1,7 @@
 import asyncio
 import discord
 from discord.ext import commands
+import datetime
 
 class Vote(commands.Cog):
     def __init__(self, client):
@@ -11,13 +12,15 @@ class Vote(commands.Cog):
         global message, yes, no
         mod = discord.utils.get(ctx.guild.roles, name="Mods")  # Gets the role "Mod" from the server
         admin = discord.utils.get(ctx.guild.roles, name=":)")  # Gets the role ":)" from the server
-        await ctx.message.delete()
         if mod in ctx.author.roles or admin in ctx.author.roles:
+            converted = str(datetime.timedelta(seconds= int(duration)))
+            await ctx.message.delete()
+            print("Done")
             output = False
-            embed = discord.Embed(title=f"Voting Duration{duration}")
+            embed = discord.Embed(title=f"Voting. (Duration {converted})")
             #embed.set_author(name=ctx.message.display_name, url=discord.Embed.Empty, icon_url=ctx.author.avatar_url)
             embed.add_field(name="Please vote on", value=vote)
-            message = await ctx.send(embed=embed)
+            message = await ctx.send("@everyone", embed=embed)
             await message.add_reaction('✅')
             await message.add_reaction("❌")
             await asyncio.sleep(int(duration))
@@ -43,6 +46,7 @@ class Vote(commands.Cog):
             await ctx.send(embed=embed2)
         else:
             response = await ctx.send("You do not have permissions to do this")
+            await ctx.message.delete()
             await asyncio.sleep(4)
             await response.delete()
 
