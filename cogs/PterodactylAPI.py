bb389c70d90197527a5caa5591b52c0249d33bbd
123hotdog1100 <123hotdog1100@gmail.com>
2021-07-19 19:21:10 +0100

    Added console commands need to make bot commands
    Signed-off-by: 123hotdog1100 <123hotdog1100@gmail.com>

--- cogs/PterodactylAPI.py
+++ cogs/PterodactylAPI.py
@@ -1,6 +1,8 @@
-from pydactyly import PterodactylClient
+from pydactyl import PterodactylClient
 import dotenv
 
 Key = dotenv.get_key(".env", "PTEROKEY")
 
-client = PterodactylClient('', Key)
+client = PterodactylClient('http://ptero.rpelliott.tk/', Key)
+def whitelist(Player)
+    client.client.send_console_command("0137f295", f"whitelist add {Player}")
